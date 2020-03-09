0963fa2d37a76bee8cba5095e81349cf49ec3504
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-03-09 19:57:41 +0530

    Update square_Q2_6-3-20.java

--- ZF - BalaSir questions/square_Q2_6-3-20.java
+++ ZF - BalaSir questions/square_Q2_6-3-20.java
@@ -1,25 +1,48 @@
-class printingSquare{
-	public static void main(String[] args) {
-		int num = 1;
-		for(int i=1;i<=16;i++){	
-			System.out.print(num+" ");
-			if(i<4 || (i>8 && i<12)){
-				num++;
-			}
-			else if((i>4 && i<8) || (i>12)){
-				num--;
-			}
-			if(i%4==0){
+import java.util.Scanner;
+
+class printingSquare
+{
+	public static void main(String[] args) 
+	{
+		Scanner inputScanner = new Scanner(System.in);
+		int num = 0;
+		int squareNumber;
+		System.out.println("Please Enter the square number:");
+		squareNumber = inputScanner.nextInt();
+		if(squareNumber>=1 && squareNumber<=100)
+		{
+			System.out.println("Snake Square numbers:");
+			for(int i=1;i<=squareNumber;i++)
+			{
+				for(int j=1;j<=squareNumber;j++)
+				{
+					if(i%2!=0)
+					{
+						num++;
+						System.out.print(num+" ");
+					}
+					else
+					{
+						System.out.print(num+" ");
+						num--;
+					}
+				}
+				num+=squareNumber;
 				System.out.println();
-				num+=4;
-			}
+			}	
 		}
+		else
+		{
+			System.out.println("please enter a number between 1 to 100");
+		}	
 	}
 }
 	
 /*
-1 2 3 4 
-8 7 6 5 
-9 10 11 12 
-16 15 14 13
+1 2 3 4 5 6 
+12 11 10 9 8 7 
+13 14 15 16 17 18 
+24 23 22 21 20 19 
+25 26 27 28 29 30 
+36 35 34 33 32 31
 */
