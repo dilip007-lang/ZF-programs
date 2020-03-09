516de79d50d3fc5b2b020ef6b2ae4264a7e45822
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-03-09 20:00:09 +0530

    Update triangle_Q1_6-3-20.java

--- ZF - BalaSir questions/triangle_Q1_6-3-20.java
+++ ZF - BalaSir questions/triangle_Q1_6-3-20.java
@@ -1,17 +1,30 @@
-class printingNumbersInTriangleShape{
-	public static void main(String[] args) {
-		for(int i=1;i<=10;i++){
-			System.out.print(i+" ");
-			if(i==4 || i==7 || i==9 || i==10){
+
+import java.util.Scanner;
+class printingNumbersInTriangleShape
+{
+	public static void main(String[] args)
+	{
+		Scanner inputScanner = new Scanner(System.in);
+		int noOfRowsAndColumns;
+		int num = 1;
+		System.out.println("Enter the number of rows that you want to form a number triagle :");
+		noOfRowsAndColumns = inputScanner.nextInt();
+		if(noOfRowsAndColumns>=1 && noOfRowsAndColumns<=100)
+		{
+			System.out.println("Numbers is triangle shape:");
+			for(int i=1;i<=noOfRowsAndColumns;i++)
+			{
+				for(int j=i;j<=noOfRowsAndColumns;j++)
+				{
+					System.out.print(num+" ");
+					num++;
+				}
 				System.out.println();
 			}
 		}
+		else
+		{
+			System.out.println("The rows should be between 1 to 100");
+		}
 	}
 }
-
-/*
-1 2 3 4 
-5 6 7 
-8 9 
-10
- */
