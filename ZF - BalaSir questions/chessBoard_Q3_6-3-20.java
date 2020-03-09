f1d0b079d5e9b4704f4409eee0e1ee7e5354590a
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-03-09 19:55:17 +0530

    Update chessBoard_Q3_6-3-20.java

--- ZF - BalaSir questions/chessBoard_Q3_6-3-20.java
+++ ZF - BalaSir questions/chessBoard_Q3_6-3-20.java
@@ -1,31 +1,157 @@
 class printingChessBoard{
 	public static void main(String[] args){
-		int a = 1;
-		for(int i=1;i<=16;i++){
-			if(a==1){
-				System.out.print(a+" ");
-				a = 0;
-			}
-			else{
-				System.out.print(a+" ");
-				a = 1;
-			}
-			if(i%4==0){
-				System.out.println();
-				if(a == 1){
-					a = 0;
+		int num = 1;
+		int noOfRows = 4;
+		int noOfColumns = 4;
+
+		/*Method - 1*/
+		for(int i=1;i<=noOfRows;i++)
+		{
+			for(int j=1;j<=noOfColumns;j++)
+			{
+				if(num==1){
+					System.out.print(num+" ");
+					//System.out.print("1 ");
+					num = 0;
 				}
 				else{
-					a = 1;
+					System.out.print(num+" ");
+					//System.out.print("0 ");
+					num = 1;
 				}
 			}
-		}
+			if(num==1){
+				num = 0;
+			}
+			else{
+				num=1;
+			}
+			System.out.println();	
+		} 
+		
+
+		/*num = 1;
+		for(int i=1;i<=noOfRows;i++)
+		{
+			for(int j=1;j<=noOfColumns;j++)
+			{
+				if(num==1){
+				System.out.print("1 ");
+				}
+				else{
+					System.out.print("0 ");
+				}
+				num*=-1;
+			}
+			num*=-1;
+			System.out.println();	
+		}*/
 	}
 }
 
-/*
-1 0 1 0 
-0 1 0 1 
-1 0 1 0 
-0 1 0 1
-*/
+
+	//System.out.println();
+		/*Method - 2*/
+		/*num = 0;
+		for(int i=1;i<=noOfRows;i++)
+		{
+			if(num==1){
+				num = 0;
+				System.out.print(num+" ");
+				num = 1;
+			}
+			else{
+				num = 1;
+				System.out.print(num+" ");
+				num = 0;
+			}
+				for(int j=2;j<=noOfColumns;j++)
+				{
+					if(num==1){
+					System.out.print(num+" ");
+					num = 0;
+					}
+					else{
+						System.out.print(num+" ");
+						num = 1;
+					}
+				}
+			System.out.println();	
+		}
+		System.out.println();*/
+
+
+
+		/*Method - 3*/
+		/*num = 0;
+		for(int i=1;i<=noOfRows;i++)
+		{
+			if(num==1){
+				System.out.print("0 ");
+				num = 1;
+			}
+			else{
+				System.out.print("1 ");
+				num = 0;
+			}
+				for(int j=2;j<=noOfColumns;j++)
+				{
+					if(num==1){
+					System.out.print("1 ");
+					num = 0;
+					}
+					else{
+						System.out.print("0 ");
+						num = 1;
+					}
+				}
+			System.out.println();	
+		} 
+		System.out.println();*/
+
+
+
+		/*Method - 4*/
+		/*num = 1;
+		for(int i=1;i<=noOfRows;i++)
+		{
+			for(int j=1;j<=noOfColumns;j++)
+			{
+				if(num==1){
+				System.out.print(num+" ");
+					if(j!=noOfColumns)
+					{
+					num = 0;
+					}
+				}
+				else{
+					System.out.print(num+" ");
+					if(j!=noOfColumns)
+					{
+					num = 1;
+					}
+				}
+			}
+			System.out.println();	
+		}
+		System.out.println();
+
+		/*Method - 5*/
+		/*num = 1;
+		for(int i=1;i<=noOfRows;i++)
+		{
+			for(int j=1;j<=noOfColumns;j++)
+			{
+				if(num==1){
+				System.out.print("1 ");
+				}
+				else{
+					System.out.print("0 ");
+				}
+				num*=-1;
+			}
+			num*=-1;
+			System.out.println();	
+		}
+		*/
+
