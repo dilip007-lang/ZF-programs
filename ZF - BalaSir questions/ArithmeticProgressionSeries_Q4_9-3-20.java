ffe14282d76eab39fd991f5f54c5deb22fce48ee
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-03-10 11:43:46 +0530

    Add files via upload

---
+++ ZF - BalaSir questions/ArithmeticProgressionSeries_Q4_9-3-20.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner;
+class ArithmeticProgressionSeries{
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+		int noOfElements;
+		int startingNumber;
+		int difference;
+		System.out.println("The number of Elements should be greater than 0 and lesser than 10001,the Starting number should be greater than or equal to 0 and Difference must be greater than 0");
+		
+		System.out.println("Enter the total number of elements :");
+		noOfElements = inputScanner.nextInt();
+
+		System.out.println("Enter the Starting number :");
+		startingNumber = inputScanner.nextInt();
+
+		System.out.println("Enter the difference :");
+		difference = inputScanner.nextInt();
+
+		if(noOfElements>=1 &&noOfElements<=10000 && startingNumber>=0 && difference>0 && difference<=10000000 && startingNumber<=10000000)
+		{
+			System.out.print(startingNumber);
+			for(int i=2;i<=noOfElements;i++)
+			{
+				startingNumber+=difference;
+				System.out.print(","+startingNumber);
+				
+			}
+			System.out.println();
+
+
+			//String outputConcat = "";
+			/*for(int i=1;i<=noOfElements;i++)
+			{	
+				outputConcat+=startingNumber+",";
+				startingNumber+=difference;
+			}
+			System.out.println(outputConcat.substring(0,outputConcat.length()-1));*/
+			//String outputSubstring = outputConcat.substring(0,outputConcat.length()-1);
+			//System.out.println(outputSubstring);
+		}
+		else
+		{
+			System.out.println("Please Enter your input correctly ");
+		}
+	}
+}
