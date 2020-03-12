19211222d46c47a4b9eb33554698a10dc0adf106
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-03-12 22:32:35 +0530

    Add files via upload

---
+++ ZF - BalaSir questions/printingTriangularPattern.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class triangularPattern
+{
+	public static void main(String[] args) 
+	{
+		Scanner inputScanner  = new Scanner(System.in);
+		System.out.print("Enter the Starting number of the triangle pattern :");
+		int userStartingNum = inputScanner.nextInt();
+		System.out.print("Enter no of rows for that triangle :");
+		int userRowAndColumn = inputScanner.nextInt();
+		int startingNum = userStartingNum;
+		int tempStartingNum = startingNum-1;
+		int numberIterator = tempStartingNum;
+		int rowAndColumn = userRowAndColumn;
+		int columnIterator = 1;
+		Boolean flag = true;
+
+		for(int i=1;i<=rowAndColumn;i++)
+		{
+			for (int j=1;j<=columnIterator;j++)
+			{
+				if(numberIterator<startingNum && flag)
+				{
+					numberIterator++;
+				}
+				else
+				{
+					flag = false;
+					numberIterator--;
+				}
+				System.out.print(numberIterator+" ");
+			}
+			flag = true;
+			startingNum++;
+			numberIterator = tempStartingNum;
+			System.out.println();
+			columnIterator+=2;
+		}
+	}
+}
