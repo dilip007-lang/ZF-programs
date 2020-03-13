16a8d1465cfcc2fd4eab3fbaab3517eccca3c64f
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-03-13 13:56:15 +0530

    Create DisariumNumberOrNot_Q12_12-3-20.java

---
+++ ZF - BalaSir questions/DisariumNumberOrNot_Q12_12-3-20.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class DisariumNumberOrNot{
+	public static void main(String[] args) {
+		Scanner inputScanner = new Scanner(System.in);
+
+		  //getting input from user
+		System.out.println("Enter the Number which you wnat to Know whether it is DISARIUM or NOT :");
+		long userInput = inputScanner.nextLong();
+		long numberToBeChecked = userInput;
+
+	  	// variable to count the no of digits
+		int digitCount = 0;
+
+	  	//the add the digits powered number
+		int sumOfTheDigits = 0;
+
+		  // this while is to calculate the no of digits
+		while(numberToBeChecked>0){
+			digitCount++;
+			numberToBeChecked/=10;
+		}
+
+		  //reEntering the users value because the number is now zero
+		numberToBeChecked = userInput;
+
+	  	// this is to add the digit by powering it with its position
+	  	// we can given the digitsCount as loop condition
+		while(numberToBeChecked>0){
+			int digit =(int)numberToBeChecked%10;
+			sumOfTheDigits+=Math.pow(digit,digitCount);
+			numberToBeChecked/=10;
+			digitCount--;
+		}
+
+
+
+		if(sumOfTheDigits==userInput){
+			System.out.println("The given number is a DISARIUM : "+sumOfTheDigits);
+		}
+		else{
+			System.out.println("The given number is NOT a DISARIUM : "+sumOfTheDigits);
+		}
+	}
+}
