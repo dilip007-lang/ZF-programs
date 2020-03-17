63497a7fdfbd41fb68dfafb77e7218c90ac83a57
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-03-17 12:15:32 +0530

    Update printingTriangularPattern.java

--- ZF - BalaSir questions/printingTriangularPattern.java
+++ ZF - BalaSir questions/printingTriangularPattern.java
@@ -44,10 +44,10 @@ class triangularPattern
 	int num = 7;
 	for(int i=1;i<=7;i++){
 		for(int j=1;j<i;j++){
-			Syste.out.print(j+" ");
+			System.out.print(j+" ");
 		}
 		for(int k=i;k>=1;k++){
-			Syste.out.print(k+" ");
+			System.out.print(k+" ");
 		}
 		System.out.println();
 	}	
