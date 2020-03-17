a337fb5f27aec64eb92fa970176978a3a5f32a76
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-03-17 12:41:34 +0530

    Update printingTriangularPattern.java

--- ZF - BalaSir questions/printingTriangularPattern.java
+++ ZF - BalaSir questions/printingTriangularPattern.java
@@ -6,20 +6,46 @@ class triangularPattern
 		Scanner inputScanner  = new Scanner(System.in);
 		System.out.print("Enter the Starting number of the triangle pattern :");
 		int userStartingNum = inputScanner.nextInt();
-		// temp variable is not needed because I'am not destroying the input
 		System.out.print("Enter no of rows for that triangle :");
 		int userRowAndColumn = inputScanner.nextInt();
-		for(int i=startingNum;i<=rowAndColumn+startingNum;i++)
+		int startingNum = userStartingNum;
+		int numberIterator = startingNum-1;
+		int columnIterator = 1;
+		Boolean flag = true;
+
+		for(int i=1;i<=userRowAndColumn;i++)
 		{
-			for(int j=startingNum;j<i;j++)
+			for (int j=1;j<=columnIterator;j++)
 			{
+				if(numberIterator<startingNum && flag)
+				{
+					numberIterator++;
+				}
+				else
+				{
+					flag = false;
+					numberIterator--;
+				}
+				System.out.print(numberIterator+" ");
+			}
+			flag = true;
+			startingNum++;
+			numberIterator =  startingNum-1;;
+			System.out.println();
+			columnIterator+=2;
+		}
+		
+		
+		/*
+		only two variables needed
+		for(int i=startingNum;i<=rowAndColumn+startingNum;i++){
+			for(int j=startingNum;j<i;j++){
 				System.out.print(j+" ");
 			}
-			for(int k=i;k>=startingNum;k--)
-			{
+			for(int k=i;k>=startingNum;k--){
 				System.out.print(k+" ");
 			}
 			System.out.println();
-		}
+		}*/
 	}
 }
