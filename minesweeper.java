3470ed43ebe0cd90c010088dbd14b20cb92d0338
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-04-08 16:49:31 +0530

    Update minesweeper.java

--- minesweeper.java
+++ minesweeper.java
@@ -1,143 +1,178 @@
 import java.util.Random;
 import java.util.Scanner;
 class minesweeperGame{
-	static int noOfRows = 8;
-	static int noOfColumns = 8;
-	static int noOfBombs = 8;
+	static int noOfRows = 4;
+	static int noOfColumns = 4;
+	static int noOfBombs = 4;
 	static int[][] minesArray;
 	static String[][] storingUserInput;
 	static Scanner inputScanner = new Scanner(System.in);
 	static int userFlagCount = noOfBombs;
 	static int noOfCrtFlags;
-	// this method is the choose the level of the game.
-	// there are three levels Easy,Medium,Hard
+	static int squaresToBeOpened = (noOfRows*noOfColumns)-noOfBombs;
+ 
 	static void gameLevel(){
 		System.out.println("Enter Game Level (Easy,Medium,Hard) :");
 		String gameLevel = inputScanner.next();
-		if(gameLevel.equals("Medium")){
+		if(gameLevel.equalsIgnoreCase("Medium")){
 			noOfRows += 4;
 			noOfColumns += 4;
 			noOfBombs += 4;
 		}
-		else if(gameLevel.equals("Hard")){
+		else if(gameLevel.equalsIgnoreCase("Hard")){
 			noOfRows += 8;
 			noOfColumns += 8;
 			noOfBombs += 8;
 		}
-		// assigning the size of the array
 		minesArray = new int[noOfRows][noOfColumns];
-		//this array is to store the value which user open or flaged a square the value will be taken from the corresponding position in minesArray and assigned here
 		storingUserInput = new String[noOfRows][noOfColumns];
+		for(int i=0;i<noOfRows;i++){
+			for(int j=0;j<noOfColumns;j++){
+				minesArray[i][j] = 0;
+				storingUserInput[i][j] = "*";
+			}
+		}
 	}
-	
-	// this method is to place numbers and bombs the first loop will place the bombs and seconf outer loop will place the numbers according to the number of bombs around it
+
+	static void conditionsForPlacingNumbers(int row,int column){
+		if((column-1>=0) && minesArray[row][column-1]!=-1){
+			minesArray[row][column-1]+=1;	
+		}
+		if((column+1<noOfColumns) && minesArray[row][column+1]!=-1){
+			minesArray[row][column+1]+=1;	
+		}
+		if((row-1>=0) && (column-1>=0) && minesArray[row-1][column-1]!=-1){
+			minesArray[row-1][column-1]+=1;	
+		}
+		if((row-1>=0) && minesArray[row-1][column]!=-1){
+			minesArray[row-1][column]+=1;	
+		}
+		if((row-1>=0) && (column+1<noOfColumns) && minesArray[row-1][column+1]!=-1){
+			minesArray[row-1][column+1]+=1;	
+		}
+		if((row+1<noOfRows) && (column-1>=0) && minesArray[row+1][column-1]!=-1){
+			minesArray[row+1][column-1]+=1;	
+		}
+		if((row+1<noOfRows) && minesArray[row+1][column]!=-1){
+			minesArray[row+1][column]+=1;	
+		}
+		if((row+1<noOfRows) && (column+1<noOfColumns) && minesArray[row+1][column+1]!=-1){
+			minesArray[row+1][column+1]+=1;	
+		}		
+	}
 	static void placingNumbersAndBombs(){
-		System.out.println("length"+minesArray.length);
 		Random randomNumber = new Random();
 		for(int i=0;i<noOfBombs;i++){
 			int rowRanNum = randomNumber.nextInt(noOfRows);
 			int columnRanNum = randomNumber.nextInt(noOfColumns);
 			if(minesArray[rowRanNum][columnRanNum]!=-1){
-				minesArray[rowRanNum][columnRanNum] = -1;
+				minesArray[rowRanNum][columnRanNum] = -1;	
 			}
 			else{
-				i-=1;
-			}	
-		}	
-		for(int i=0;i<noOfRows;i++){
-			for(int j=0;j<noOfColumns;j++){
-				if(minesArray[i][j]==-1){
-					if( i>=0 && j>=0 && i<noOfRows && j<noOfColumns){
-						if((j-1>=0) && minesArray[i][j-1]!=-1){
-							minesArray[i][j-1]+=1;	
-						}
-						if((j+1<noOfColumns) && minesArray[i][j+1]!=-1){
-							minesArray[i][j+1]+=1;	
-						}
-						if((i-1>=0) && (j-1>=0) && minesArray[i-1][j-1]!=-1){
-							minesArray[i-1][j-1]+=1;	
-						}
-						if((i-1>=0) && minesArray[i-1][j]!=-1){
-							minesArray[i-1][j]+=1;	
-						}
-						if((i-1>=0) && (j+1<noOfColumns) && minesArray[i-1][j+1]!=-1){
-							minesArray[i-1][j+1]+=1;	
-						}
-						if((i+1<noOfRows) && (j-1>=0) && minesArray[i+1][j-1]!=-1){
-							minesArray[i+1][j-1]+=1;	
-						}
-						if((i+1<noOfRows) && minesArray[i+1][j]!=-1){
-							minesArray[i+1][j]+=1;	
-						}
-						if((i+1<noOfRows) && (j+1<noOfColumns) && minesArray[i+1][j+1]!=-1){
-							minesArray[i+1][j+1]+=1;	
-						}
-					}
+				if(rowRanNum<noOfRows-1 && minesArray[rowRanNum+1][columnRanNum]!=-1){
+					minesArray[rowRanNum+1][columnRanNum] = -1;
+					rowRanNum+=1;
+				}
+				else if(columnRanNum<noOfColumns-1 && minesArray[rowRanNum][columnRanNum+1]!=-1){
+					minesArray[rowRanNum][columnRanNum+1] = -1;
+					columnRanNum+=1;
+				}
+				else if(columnRanNum>0 && minesArray[rowRanNum][columnRanNum-1]!=-1){
+					minesArray[rowRanNum][columnRanNum-1] = -1;
+					columnRanNum-=1;
+				}
+				else if(rowRanNum>0 && minesArray[rowRanNum-1][columnRanNum]!=-1){
+					minesArray[rowRanNum-1][columnRanNum] = -1;
+					rowRanNum-=1;
 				}
 			}
-		}
+			conditionsForPlacingNumbers(rowRanNum,columnRanNum);						
+		}	
 	}
-	// this method is to check the condition for opening a square	
+
 	static String checkingSquareOpeningCondition(int row,int column){
 		String returnValue = "";
-		if(minesArray[row][column] == -1){
+		squaresToBeOpened--;
+		if(squaresToBeOpened!=0){
+			if(minesArray[row][column] == -1){
+			storingUserInput[row][column] = "-1";
 			returnValue =  "Loss";
-		}
-		else if(minesArray[row][column] == 0){
-			for(int i=0;i<noOfRows;i++){
-				for(int j=0;j<noOfColumns;j++){
-					if(minesArray[i][j] == 0){
-						storingUserInput[i][j] = Integer.toString(minesArray[i][j]);
+			}
+			else if(minesArray[row][column] == 0){
+				squaresToBeOpened++;
+				for(int i=0;i<noOfRows;i++){
+					for(int j=0;j<noOfColumns;j++){
+						if(minesArray[i][j] == 0){
+							storingUserInput[i][j] = Integer.toString(minesArray[i][j]);
+							squaresToBeOpened--;
+						}
 					}
 				}
 			}
-			returnValue = "Opened";
+			else{
+				storingUserInput[row][column] = Integer.toString(minesArray[row][column]);
+			}
 		}
-		else{
-			storingUserInput[row][column] = Integer.toString(minesArray[row][column]);
+
+		else if(squaresToBeOpened==0){
+			if(minesArray[row][column] == -1){
+				storingUserInput[row][column] = "-1";
+				returnValue =  "Loss";
+			}
+			else{
+				storingUserInput[row][column] = Integer.toString(minesArray[row][column]);
+			}
+			if(userFlagCount==0 && noOfCrtFlags==noOfBombs){
+				returnValue = "Won";
+			}
+			else if(userFlagCount==0 && noOfCrtFlags<noOfBombs){
+				returnValue = "Loss";
+			}	
 		}
-		System.out.println(storingUserInput[row][column]);
 		return returnValue;
 	}
-	
-	// this method is to check the condition to flag a square
+
 	static String checkingSquareFlagCondition(int row,int column){
 		String returnValue = "";
-		System.out.println(minesArray[row][column]);
-		if(minesArray[row][column] == -1){
+		userFlagCount--;		
+		if(userFlagCount>0){
 			storingUserInput[row][column] = "F";
-			System.out.println(storingUserInput[row][column]);
-			noOfCrtFlags++;	
-			if(noOfCrtFlags==noOfBombs){
-				returnValue = "Won";
-				return returnValue;
+			if(minesArray[row][column] == -1){
+				noOfCrtFlags++;	
 			}
-			if(userFlagCount>0){
-				userFlagCount--;
-			}		
-			else{
+		}
+		else if(userFlagCount==0){
+			storingUserInput[row][column] = "F";
+			if(minesArray[row][column] == -1){
+				noOfCrtFlags++;	
+			}
+			if(squaresToBeOpened==0 && noOfCrtFlags==noOfBombs){
+				returnValue = "Won";
+			}
+			else if(squaresToBeOpened==0 && noOfCrtFlags<noOfBombs){
 				returnValue = "Loss";
 			}
 		}
 		else{
-			storingUserInput[row][column] = "F";
-			userFlagCount--;
-			returnValue = "Flagged";
-		}
+			System.out.println("You have placed all of your flags");
+		}	
 		return returnValue;
 	}
-	
-	// this is the method which invokes the above two method to get input and check the input and render result
+
 	static boolean checkingInputValues(int row,int column,String squareType){
 		String openedSquare = "";
 		String flaggedSquare = "";
 		boolean returnValue = false;
-		if(row>=0 && row<noOfRows && column>=0 && column<noOfColumns && (squareType.equals("F") || squareType.equals("O"))){
+		if(row>=0 && row<noOfRows && column>=0 && column<noOfColumns && (squareType.equalsIgnoreCase("F") || squareType.equalsIgnoreCase("O"))){
 			if(storingUserInput[row][column] == "*"){
-				if(squareType.equals("O")){
+				if(squareType.equalsIgnoreCase("O")){
 					openedSquare = checkingSquareOpeningCondition(row,column);
 					if(openedSquare.equals("Loss")){
 						System.out.println("You Loss the game ....");
+						returnValue = true;
+					}
+					else if(openedSquare.equals("Won")){
+						System.out.println("You Won the game ....");
 						returnValue = true;
 					}
 				}
@@ -163,17 +198,11 @@ class minesweeperGame{
 		}
 		return returnValue;
 	}
-	
+
 	static void gettingInputAndShowingResult(){
-		int noOfFlags = noOfBombs;
-		for(int i=0;i<noOfRows;i++){
-			for(int j=0;j<noOfColumns;j++){
-				storingUserInput[i][j] = "*"; 
-			}
-		}
 		boolean startGame = true;
 		while(startGame){
-			System.out.println("Flag remaining :"+noOfFlags+"\n");
+			System.out.println("Flag remaining :"+userFlagCount+"\n");
 			for(String[] innerArray:storingUserInput){
 				for(String arrValues:innerArray){
 					System.out.print(arrValues+"\t");
@@ -191,11 +220,9 @@ class minesweeperGame{
 			}
 		}
 	}
-	
 	public static void main(String[] args) {
 		gameLevel();	
 		placingNumbersAndBombs();
-		// just for checking
 		for(int[] innerArray : minesArray){
 			for(int values : innerArray){
 				System.out.print(values+"\t");	
