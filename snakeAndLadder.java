b3c7790f68478f43b9456496e7f9f83e2526f181
dilip007-lang <61870600+dilip007-lang@users.noreply.github.com>
2020-04-22 15:36:25 +0530

    Update snakeAndLadder.java

--- snakeAndLadder.java
+++ snakeAndLadder.java
@@ -1,11 +1,39 @@
 import java.util.Random;
 import java.util.HashMap;
 import java.util.Scanner;
+
+class playerInfo{
+	private String playerName;
+	private int playerCurrentPosition;
+	private int playerPreviousPosition;
+	public void setPlayerName(String playerName){
+		this.playerName = playerName;
+	}
+	public void setPlayerCurrentPosition(int playerCurrentPosition){
+		this.playerCurrentPosition = playerCurrentPosition;
+	}
+	public void setPlayerPreviousPosition(int playerPreviousPosition){
+		this.playerPreviousPosition = playerPreviousPosition;
+	}
+	public String getPlayerName(){
+		return playerName;
+	}
+	public int getPlayerCurrentPosition(){
+		return playerCurrentPosition;
+	}
+	public int getPlayerPreviousPosition(){
+		return playerPreviousPosition;
+	}
+}
+
 class snakeAndLadder{
-	private static int[][] gameBoard = new int[10][10];
+	private static String[][] stringGameBoard = new String[10][10];
+	private static int[][]	gameBoard = new int[10][10];
 	private static HashMap<Integer,Integer> snakePosition = new HashMap<>();
 	private static HashMap<Integer,Integer> LadderPosition = new HashMap<>();
+	private static playerInfo playersObjArr[];
 	static Scanner inputScanner = new Scanner(System.in);
+	private static int finishedPlayers = 0;
 	private static boolean gameEnds = false;
 	static void setSnakeAndLadderPosition(){
 		snakePosition.put(32,10);
@@ -15,7 +43,6 @@ class snakeAndLadder{
 		snakePosition.put(88,24);
 		snakePosition.put(95,56);
 		snakePosition.put(97,78);
-
 		LadderPosition.put(1,38);
 		LadderPosition.put(4,14);
 		LadderPosition.put(8,30);
@@ -23,174 +50,173 @@ class snakeAndLadder{
 		LadderPosition.put(28,76);
 		LadderPosition.put(50,67);
 		LadderPosition.put(71,92);
-		LadderPosition.put(80,99);		
+		LadderPosition.put(80,99);
 	}
 
-	static void assigningValuesToGameBoard(){		
-		int num1 = 1;
+
+	static void assigningValuesToGameBoard(){
+		int num1 = 100;
+		int increasingValue = 1;
 		for(int i=0;i<gameBoard.length;i++){
 			for(int j=0;j<gameBoard.length;j++){
 				gameBoard[i][j] = num1;
-				num1++;
+				stringGameBoard[i][j] = Integer.toString(num1);
+				num1-=increasingValue;
 			}
-			i++;
-			num1+=10;
-			for(int k=0;k<gameBoard.length;k++){
-				num1--;
-				gameBoard[i][k] = num1;	
-			}
-			num1+=10;
+			increasingValue = increasingValue*(-1);
+			num1-=10+(increasingValue);
 		}
-		/*int  value = 0;
-		// It takes more time then the other two types
-		for(int i=0;i<gameBoard.length;i++){
-			for(int j=0;j<gameBoard.length;j++){
-				if(i%2==0){
-					value++;
-					gameBoard[i][j] = value;
-				}
-				else{
-					gameBoard[i][j] = value;
-					value--;
-				}
-			}
-			value+=10;
-		}*/
-	/*int num1 = 0;
-		for(int i=0;i<gameBoard.length;i+=2){
-			for(int j=0;j<gameBoard.length;j++){
-				num1++;
-				gameBoard[i][j] = Integer.toString(num1);
-			}
-			num1+=10;
-		}
-		num1 = 21;
-		for(int i=1;i<gameBoard.length;i+=2){
-			for(int j=0;j<gameBoard.length;j++){
-				num1--;
-				gameBoard[i][j] = Integer.toString(num1);
-			}
-			num1+=30;
-		}*/
 	}
+
 
 	static int generatingRandomNum(){
 		Random ran = new Random();
 		return ran.nextInt(6)+1;
 	}
 
-	static int checkingSnakeAndLadderPosition(int playerPosition,String currentPlayer){
-		if(snakePosition.containsKey(playerPosition)){
-			playerPosition = snakePosition.get(playerPosition);
-			System.out.println("Sorry! A Snake bites "+currentPlayer+" ....");			
+
+	static int gettingTotalNoOfPlayers(){
+		System.out.println("Enter total Number of players ...");
+		int totalPlayers = inputScanner.nextInt();
+		if(totalPlayers<2){
+			gettingTotalNoOfPlayers();	
 		}
-		else if(LadderPosition.containsKey(playerPosition)){
-			playerPosition = LadderPosition.get(playerPosition);
-			System.out.println(playerPosition);
-			System.out.println("Wow ! "+currentPlayer+" got a Ladder ....");
-		}
-		return playerPosition;
+		playersObjArr = new playerInfo[totalPlayers];
+			System.out.println(playersObjArr.length);
+		assigningNameToPlayers();
+		return totalPlayers;
 	}
 
-	static int assigningPlayerPosition(int currentPosition,String currentPlayer){
+
+	static void assigningNameToPlayers(){
+		for(int i=0;i<playersObjArr.length;i++){
+			playersObjArr[i] = new playerInfo();
+			playersObjArr[i].setPlayerName("P"+(i+1)); 
+		}
+	}
+
+	static void checkingWinningCondition(playerInfo playerObj){
+		if(playerObj.getPlayerCurrentPosition()==100){
+			finishedPlayers+=1;
+			System.out.println(playerObj.getPlayerName()+" Finished the Game ....");
+		}
+	}
+
+	static void displayGameBoard(){
+		for(int i=0;i<stringGameBoard.length;i++){
+			for(int j=0;j<stringGameBoard.length;j++){
+				System.out.print(stringGameBoard[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+	static void checkingSnakeAndLadderPosition(playerInfo playerObj){
+		int position = playerObj.getPlayerCurrentPosition();
+		System.out.println("current position "+position);
+		String playerName = playerObj.getPlayerName();
+		if(snakePosition.containsKey(playerObj.getPlayerCurrentPosition())){
+			System.out.println("snake bites");
+			playerObj.setPlayerCurrentPosition(snakePosition.get(position));
+			System.out.println("Sorry! A Snake bites "+playerName+" ....");			
+		}
+		else if(LadderPosition.containsKey(playerObj.getPlayerCurrentPosition())){
+			System.out.println("got ladder");
+			playerObj.setPlayerCurrentPosition(LadderPosition.get(position));
+			System.out.println("Wow ! "+playerName+" got a Ladder ....");
+		}
+	}
+
+	static void changingValuesInGameBoard(playerInfo playerObj){
+		String playerName = playerObj.getPlayerName();
+		int currentPos = playerObj.getPlayerCurrentPosition();
+		int previousPos = playerObj.getPlayerPreviousPosition();
+		for(int i=0;i<gameBoard.length;i++){
+			for(int j=0;j<gameBoard.length;j++){
+				if(gameBoard[i][j] == currentPos){
+					if(stringGameBoard[i][j].startsWith("P")){
+						stringGameBoard[i][j] += playerName;
+					}
+					else{
+						stringGameBoard[i][j] = playerName;
+					}
+					
+				}
+				else if(gameBoard[i][j] == previousPos){
+					if(stringGameBoard[i][j].equals(playerName)){
+						stringGameBoard[i][j] = Integer.toString(previousPos);
+					}
+					else if(stringGameBoard[i][j].contains(playerName)){
+						String replacedWord = stringGameBoard[i][j].replace(playerName,"");
+						stringGameBoard[i][j] = replacedWord;
+					}
+				}
+
+			}
+		}
+	}
+
+	
+	static void assigningPlayerPosition(playerInfo playerObj){
 		int ranNum = 0;
-		int playerPosition = currentPosition;
-		String boardReturnValue = "";
+		int currentPosition = playerObj.getPlayerCurrentPosition();
+		String playerName = playerObj.getPlayerName();
+		System.out.println(playerName);
 		do{
 			ranNum = generatingRandomNum();
-			if(ranNum==6){
-				System.out.println("Wow ! "+currentPlayer+" got an Extra dice  dice number is "+ranNum);
-			} 
-			else{
-				System.out.println(""+currentPlayer+" dice number is "+ranNum);
-			}
-			playerPosition+=ranNum;
-			if(playerPosition>100){
-				System.out.println("Sorry Your dice can't be moved because the dice number is going greater that 100");
-				playerPosition = playerPosition-ranNum;
+			if((currentPosition+ranNum)>100){
+				System.out.println("Sorry Your position can't be moved because the dice number is going greater that 100");
 			}
 			else{
-				playerPosition = checkingSnakeAndLadderPosition(playerPosition,currentPlayer);
+				playerObj.setPlayerPreviousPosition(currentPosition);
+				currentPosition = currentPosition+ranNum;
+				playerObj.setPlayerCurrentPosition(currentPosition);
+				checkingSnakeAndLadderPosition(playerObj);
+				changingValuesInGameBoard(playerObj);
+				if(ranNum==6){
+					System.out.println("Wow ! "+playerName+" got an Extra dice because he got a : "+ranNum);
+					System.out.println(""+playerObj.getPlayerName()+" is playing ..");
+					System.out.println("Type 'roll' to ROLL the dice");
+					continue;
+				} 
+				else{
+					System.out.println(""+playerName+" dice number is : "+ranNum);
+				}
 			}
-			
 		}
 		while(ranNum==6);
-		return playerPosition;
 	}
 
-	static int gettingDiceValueAndChecking(int currentPlayerPosition,String playerName){
-		int playerPosition = currentPlayerPosition;
-		System.out.println(""+playerName+" is playing ..");
+	static void gettingDiceValueAndChecking(playerInfo playerObj){
+		System.out.println(""+playerObj.getPlayerName()+" is playing ..");
 		System.out.println("Type 'roll' to ROLL the dice");
 		if(inputScanner.next().equalsIgnoreCase("roll")){
-			playerPosition = assigningPlayerPosition(playerPosition,playerName);
+			assigningPlayerPosition(playerObj);
+			displayGameBoard();
+			checkingWinningCondition(playerObj);
 		}
 		else{
 			System.out.println("Game ends ..");
-			System. exit(1);
-		}
-		checkingWinningCondition(playerPosition,playerName);
-		return playerPosition;
-	}
-
-	static void checkingWinningCondition(int playerPosition,String playerName){
-		if(playerPosition==100){
-			System.out.println(playerName+" Won the Game ....");
-			gameEnds = true;
-		}
-	}
-
-	static void printingGameBoard(int player1Position,String player1Name,int player2Position,String player2Name){
-		for(int i=gameBoard.length-1;i>=0;i--){
-			for(int j=0;j<gameBoard.length;j++){
-				if(gameBoard[i][j] == player1Position){
-					System.out.print(player1Name+"\t\t");
-				}
-				else if(gameBoard[i][j] == player2Position){
-					System.out.print(player2Name+"   \t");
-				}
-				else{
-					System.out.print(gameBoard[i][j]+"\t\t");
-				}
-			}
-			System.out.println("\n");
+			System.exit(1);
 		}
 	}
 
 	static void processingGame(){
-				int player1Position = 0;
-		int player2Position = 0;
-		System.out.print("Welcome to Snake and Ladder ....");
-		System.out.println("Do you want to play with our 'Computer' or an another 'Player' (type 'com' or 'ply2')");
-		switch(inputScanner.next()){
-			case "com":
-			case "computer":
-				while(!gameEnds){
-					player1Position = gettingDiceValueAndChecking(player1Position,"Player1");
-					player2Position = assigningPlayerPosition(player2Position,"Computer");
-					checkingWinningCondition(player1Position,"Computer");
-					printingGameBoard(player1Position,"Player",player2Position,"Computer");
-				}
-
-				break;
-			case "ply2":
-			case "player2":
-				while(!gameEnds){
-					player1Position = gettingDiceValueAndChecking(player1Position,"Player1");
-					player2Position = gettingDiceValueAndChecking(player2Position,"Player2");	
-					printingGameBoard(player1Position,"Player1",player2Position,"player2");
-				}
-				break;
-			default:
-				System.out.println("Sorry you didn't gave correct input try again ....");
-				processingGame();				
+		System.out.println("Welcome to Snake and Ladder game ....");
+		int totalNoOfPlayers = gettingTotalNoOfPlayers();
+		System.out.println(totalNoOfPlayers+"d");
+		System.out.println(finishedPlayers);
+		while(finishedPlayers<=totalNoOfPlayers-1){
+			for(int i=0;i<totalNoOfPlayers;i++){
+				gettingDiceValueAndChecking(playersObjArr[i]);
+			}
 		}
+		System.out.println("Game ends");
 	}
 
 	public static void main(String[] args) {
 		assigningValuesToGameBoard();
 		setSnakeAndLadderPosition();
 		processingGame();
-	
 	}
 }
